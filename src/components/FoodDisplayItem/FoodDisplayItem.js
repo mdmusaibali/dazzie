a8b27522eabac63ca4bf1ebe1d8b940cde7f98de
Mohammed Musaib Ali <64194019+mdmusaibali@users.noreply.github.com>
2022-08-17 22:20:05 +0530

    Adding alt to images

--- src/components/FoodDisplayItem/FoodDisplayItem.js
+++ src/components/FoodDisplayItem/FoodDisplayItem.js
@@ -4,7 +4,7 @@ export const FoodDisplayItem = ({ foodItem }) => {
   return (
     <div className={foodItem.trending ? "FoodTrending" : "FoodDisplayItem"}>
       <div className="FoodDisplayItem__image">
-        <img src={foodItem.imageLoc} />
+        <img src={foodItem.imageLoc} alt={`${foodItem.name}`} />
       </div>
       <div className="FoodDisplayItem__name">{foodItem.name}</div>
       <div className="FoodDisplayItem__price">${foodItem.price}</div>
